53bd06bfbb20d406e4ca9f8762cf1cdc8185027a
lyndontavares <integraldominio@gmail.com>
2021-11-08 23:17:31 -0300

    *

--- aula-2/basic/src/app/app.module.ts
+++ aula-2/basic/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HelloComponent } from './04-angular-test-bed/hello.component';
 
 import { AppComponent } from './app.component';
-import { CounterPropertyBindingComponent } from './05-property-binding/counter-property-binding/counter-property-binding.component';
 import { FilterPipe } from './06-pipe/filter.pipe';
+import { CounterPropertyBindingComponent } from './05-property-binding/counter-property-binding.component';
 
 @NgModule({
   declarations: [
