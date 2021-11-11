30ce425f6c79398416d9063cafab8c2d81a0f29e
lyndontavares <integraldominio@gmail.com>
2021-11-11 11:05:34 -0300

    *

--- aula-2/basic/src/app/15-promise-observable/teste.service.spec.ts
+++ aula-2/basic/src/app/15-promise-observable/teste.service.spec.ts
@@ -4,7 +4,7 @@ import { TesteService } from "./teste.service";
     Teste de Jasmine direto sem o suporte de teste do Angular
 */
 
-describe('ValueService', () => {
+describe('promise-observable', () => {
 
     const nome = 'Lyndon Tavares'
     let service: TesteService;
@@ -29,4 +29,47 @@ describe('ValueService', () => {
                 done();
             });
         });
+
+    /*
+        https://testing-angular.com/testing-services/#testing-a-service-with-internal-state
+
+        ❏ BehaviorSubject emite o valor atual para novos assinantes de forma síncrona
+
+    */
+
+    it('#getNomeFromBehaviorSubject deve retornar Lyndon Tavares do BehaviorSubject',
+        () => {
+            service.getNomeFromBehaviorSubject().subscribe(value => {
+                expect(value).toBe(nome);
+            })
+            .unsubscribe();
+            /*
+
+             A Primeira Regra dos Observáveis ​​RxJS afirma: “Qualquer pessoa que se inscreve, deve cancelar a inscrição também”
+             (reduz inscriçoes indesejadas)
+
+            */
+        }
+    )
+
+    it('#getNomeFromBehaviorSubject deve retornar Superman do BehaviorSubject',
+        () => {
+            service.setNomeToBehaviorSubject('Superman');
+            service.getNomeFromBehaviorSubject().subscribe(value => {
+                expect(value).toBe('Superman');
+            })
+            .unsubscribe();
+        }
+    )
+
+    it('#setNomeToSubject deve retornar Batman do Subject',
+        () => {
+            service.setNomeToSubject('Batman');
+            service.getNomeFromSubject().subscribe(value => {
+                console.log(value);
+                expect(value).toBe('Batman');
+            })
+        }
+    )
+
 });
--- aula-2/basic/src/app/15-promise-observable/teste.service.ts
+++ aula-2/basic/src/app/15-promise-observable/teste.service.ts
@@ -1,8 +1,10 @@
-import { lastValueFrom, Observable, of } from "rxjs";
+import { BehaviorSubject, lastValueFrom, Observable, of, Subject } from "rxjs";
 
 export class TesteService {
 
     private nome = 'Lyndon Tavares'
+    private $nome = new BehaviorSubject<string>('Lyndon Tavares')
+    private $nomeSub = new Subject<string>()
 
     getNomeFromString(): string {
         return this.nome;
@@ -16,4 +18,21 @@ export class TesteService {
     getNomeFromObservable(): Observable<string> {
         return of(this.nome)
     }
+
+    getNomeFromBehaviorSubject(): Observable<string> {
+        return this.$nome.asObservable()
+    }
+
+    setNomeToBehaviorSubject(nome) : void {
+        this.$nome.next(nome)
+    }
+
+    getNomeFromSubject(): Observable<string> {
+        return this.$nomeSub.asObservable()
+    }
+
+    setNomeToSubject(nome) : void {
+        this.$nomeSub.next(nome)
+    }
+
 }
